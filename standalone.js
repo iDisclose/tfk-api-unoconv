d42cb178c579efa5af2bebc58780415c708eb6d9
Tom Locke <tom@tomlocke.com>
2017-12-06 16:19:17 -0400

    Fix shell-out to unoconv

--- standalone.js
+++ standalone.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var spawn = require('child_process').spawn;
-spawn('node', ['/usr/bin/unoconv', '--listener', '--server=0.0.0.0', '--port=2002'], {detached: true}).unref()
+spawn('/usr/bin/unoconv', ['--listener', '--server=0.0.0.0', '--port=2002'], {detached: true}).unref()
 
 const server = require('./server')
 server.start()
