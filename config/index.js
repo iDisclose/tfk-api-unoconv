573c8e363624da7d43dcb2db4310ac7d16ba497c
Tom Locke <tom@tomlocke.com>
2017-12-06 16:09:25 -0400

    Use $PORT for server port

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 3000,
+  SERVER_PORT: process.env.PORT || 3000,
   PAYLOAD_MAX_SIZE: process.env.PAYLOAD_MAX_SIZE || 1048576,
   TIMEOUT_SERVER: process.env.TIMEOUT_SERVER || 60000 * 2,
   TIMEOUT_SOCKET: process.env.TIMEOUT_SOCKET || 70000 * 2
